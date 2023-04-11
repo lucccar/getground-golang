c0fec91a67b2837c97f71b60e382b608dce5233a
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2023-04-10 22:19:47 -0300

    adding interface to database implementation

--- cmd/Config/config.go
+++ cmd/Config/config.go
@@ -10,23 +10,29 @@ import (
 
 const (
 	dbDriver = "mysql"
-	dbUser = "gosql"
-	dbPass = "root"
-	dbName = "goserver"
-
-	
+	dbUser   = "gosql"
+	dbPass   = "root"
+	dbName   = "goserver"
 )
-func Connect() *sql.DB {
-	
+
+type DBConnector interface {
+	Close() error
+	Exec(query string, args ...any) (sql.Result, error)
+	QueryRow(query string, args ...any) *sql.Row
+	Query(query string, args ...any) (*sql.Rows, error)
+}
+
+func Connect() DBConnector {
+
 	USER := os.Getenv("DB_USER")
 	PASS := os.Getenv("DB_PASSWORD")
 	HOST := os.Getenv("DB_HOST")
 	DBNAME := os.Getenv("DB_NAME")
 	PORT := os.Getenv("DB_PORT")
-	
-	connStr:=USER+":"+PASS+"@tcp("+HOST+":"+PORT+")/"+DBNAME
+
+	connStr := USER + ":" + PASS + "@tcp(" + HOST + ":" + PORT + ")/" + DBNAME
 	// USER+":"+PASS+"@/"+DBNAME
-    
+
 	db, err := sql.Open(dbDriver, connStr)
 	if err != nil {
 		log.Fatal(err)
